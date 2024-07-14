f11988e99795592ca1c7aecbe614e40295875dd7
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2024-07-14 23:43:22 +0200

    Misc improvements

--- Programs/Python/test/RIPO.py
+++ Programs/Python/test/RIPO.py
@@ -4,14 +4,14 @@ import numpy as np
 
 from MIDAS.enums import *
 from MIDAS.polar_grid import PolarGrid
-from MIDAS.coefficients import Coefficients
 from MIDAS.engine import Engine
 
 os.system('clear')
 
 # === Parameters ===========================================================
 
-# movieDir = project.root + '/Movies/'
+dataDir = '/home/raphael/Science/Projects/CM/MovingAgents/Data/'
+movieDir = '/home/raphael/Science/Projects/CM/MovingAgents/Movies/'
 
 # === Engine ===============================================================
 
@@ -28,7 +28,7 @@ E.steps = None
 #  --- Inputs
 
 # polar grid
-G = PolarGrid(rZ=[], nSa = 4)
+G = PolarGrid(rZ=[], nSa=4)
 
 # in_presence = E.add_input(Perception.PRESENCE,
 #                           normalization = Normalization.SAME_GROUP,
@@ -46,41 +46,43 @@ out_da = E.add_output(Action.REORIENTATION,
 # out_dv = E.add_output(Action.SPEED_MODULATION,
 #                       activation = Activation.HSM_CENTERED)
 
-# --- Groups
+net = E.add_network(inputs=[in_orientation], outputs=[out_da])
 
-# Initial conditions
-N = 100
-IC = {'position': None,
-      'orientation': None,
-      'speed': 0.01}  
+print(net)
 
-E.add_group(Agent.RIPO, N, name='agents',
-            initial_condition = IC,
-            inputs=[in_orientation], outputs=[out_da])
+# # --- Groups
 
-# --- Coefficients
+# # Initial conditions
+# N = 100
+# IC = {'position': None,
+#       'orientation': None,
+#       'speed': 0.01}  
 
-# E.set_coefficients(in_presence, np.array([1, 1, 1, 1]))
-E.set_coefficients(in_orientation, np.array([1, 1, 1, 1])*0.1)
+# E.add_group(Agent.RIPO, N, name='agents',
+#             initial_condition = IC)
 
-# C = np.array([1,1,1,1, 0, 0, 0, 0])*2
+# # --- Coefficients
 
-# === Storage ==============================================================
+# # E.set_coefficients(in_presence, np.array([1, 1, 1, 1]))
+# E.set_coefficients(in_orientation, np.array([1, 1, 1, 1])*0.1)
 
-# E.setup_storage('/home/raphael/Science/Projects/CM/MovingAgents/Data/RIPO/test.db')
+# # C = np.array([1,1,1,1, 0, 0, 0, 0])*2
 
-# E.storage.db_commit_each_step = True
+# # === Storage ==============================================================
 
-# === Visualization ========================================================
+# # E.setup_storage(dataDir + 'RIPO/test.db')
+# # E.storage.db_commit_each_step = True
 
-E.setup_animation(agents=AnimAgents.SUBSET_100, field=AnimField.DENSITY)
-E.animation.trace_duration = 10
-# E.animation.group_options['agents']['cmap'] = 'hsv'
-E.animation.field_options['range'] = [0, 1]
+# # === Visualization ========================================================
 
-# === Simulation ===========================================================
+# E.setup_animation(agents=AnimAgents.SUBSET_100, field=AnimField.DENSITY)
+# E.animation.trace_duration = 10
+# # E.animation.group_options['agents']['cmap'] = 'hsv'
+# E.animation.field_options['range'] = [0, 1]
 
-# E.window.autoplay = False
-# E.window.movieFile = movieDir + 'test.mp4'
+# # === Simulation ===========================================================
 
-E.run()
+# # E.window.movieFile = movieDir + 'test.mp4'
+
+# # E.window.autoplay = False
+# E.run()
